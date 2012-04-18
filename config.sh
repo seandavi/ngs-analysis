2a0e8a2d6cc3c81bfa53262ba26c9492017f0529
Joseph Kim <jjinking@gmail.com>
2012-04-18 14:53:51 +0900

    Finished wrapper for cutadapt.  Still need to test on real data

--- config.sh
+++ config.sh
@@ -1,35 +1,12 @@
 #!/bin/bash
 
 ###################################################################################
-# SET ENVIRONMENT VARIABLES
+# SET UP WORKSPACE
 
-# Relative to NGS_ANALYSIS_DIR
+# Set environment variables
 export NGS_ANALYSIS_DIR=`pwd`
 export PATH=$PATH:$NGS_ANALYSIS_DIR/scripts
 export PYTHONPATH=$PYTHONPATH:$NGS_ANALYSIS_DIR/lib/python
-
-# Programs
-export PYTHON=path/to/python
-export GATK=path/to/GenomeAnalysisTK.jar
-#export CUTADAPT=path/to/
-#export SICKLE=path/to/
-#export BWA=path/to/
-#export PICARD_PATH=path/to/
-#export VARSCAN=path/to/
-#export SOMATIC_SNIPER=path/to/
-#export PLINK=path/to/
-
-# Resources
-export REF=path/to/human_g1k_v37.fasta
-export UCSC_REFFLAT=path/to/refFlat.txt
-export UCSC_DBSNP=path/to/snp132.txt
-export DBSNP_VCF=path/to/dbsnp_132.b37.vcf
-export MILLS_DEVINE_INDEL_VCF=path/to/Mills_Devine_2hit.indels.b37.sites.vcf
-export HAPMAP_VCF=path/to/hapmap_3.3.b37.sites.vcf
-export OMNI1000_VCF=path/to/1000G_omni2.5.b37.sites.vcf
-
-###################################################################################
-# SET UP WORKSPACE
 
 # Import bash function library
 source $NGS_ANALYSIS_DIR/lib/bash/bash_fnc.sh
@@ -38,6 +15,36 @@ source $NGS_ANALYSIS_DIR/lib/bash/bash_fnc.sh
 create_dir data
 create_dir tmp
 create_dir reports
+
+###################################################################################
+# EXPERIMENT RUN INFORMATION
+export SAMPLESHEET=path/to/SampleSheet.csv
+export READLENGTH=101
+export PAIRED=true # [true|false]
+
+###################################################################################
+# PROGRAMS
+export PYTHON=path/to/python
+export CUTADAPT=path/to/cutadapt
+#export SICKLE=path/to/
+#export BWA=path/to/
+#export PICARD_PATH=path/to/
+export GATK=path/to/GenomeAnalysisTK.jar
+#export VARSCAN=path/to/
+#export SOMATIC_SNIPER=path/to/
+#export PLINK=path/to/
+
+###################################################################################
+# RESOURCES
+export REF=path/to/human_g1k_v37.fasta
+export UCSC_REFFLAT=path/to/refFlat.txt
+export UCSC_DBSNP=path/to/snp132.txt
+export DBSNP_VCF=path/to/dbsnp_132.b37.vcf
+export MILLS_DEVINE_INDEL_VCF=path/to/Mills_Devine_2hit.indels.b37.sites.vcf
+export HAPMAP_VCF=path/to/hapmap_3.3.b37.sites.vcf
+export OMNI1000_VCF=path/to/1000G_omni2.5.b37.sites.vcf
+
+
 
 
 
--- modules/seq/cutadapt.sh
+++ modules/seq/cutadapt.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
+##
+## DESCRIPTION: Cut adapter sequences from a fastq file
+## 
+## USAGE: cutadapt.sh input.fastq.gz adaptor_sequence
+##
+## OUTPUT: input.cutadapt.fastq.gz
+##
 
+# Load bash function library
+source $NGS_ANALYSIS_DIR/lib/bash/bash_fnc.sh
 
-echo $REF
+# Check correct usage
+usage 2 $# $0
+
+# Format output filenames
+OUTPUTPREFIX=`filter_ext $1 2`.cutadapt
+OUTPUTFILE=$OUTPUTPREFIX.fastq.gz
+OUTPUTSUMMARY=$OUTPUTPREFIX.summary
+
+# Run tool
+$CUTADAPT                      \
+	-o $OUTPUTFILE             \
+	-b $2                      \
+	-e 0.1                     \
+	-q 10                      \
+	-O 5                       \
+	$1                         \
+	>& $OUTPUTSUMMARY
