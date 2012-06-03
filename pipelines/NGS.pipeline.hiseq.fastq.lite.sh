90d0127a3e8468dd176743f1359808f8ccbf0997
Joseph Kim <jjinking@gmail.com>
2012-06-03 23:04:29 +0900

    Modified pipelines/NGS.pipeline.hiseq.fastq.lite.sh pipelines/NGS.pipeline.hiseq.fastq.sh

--- pipelines/NGS.pipeline.hiseq.fastq.lite.sh
+++ pipelines/NGS.pipeline.hiseq.fastq.lite.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 ##
 ## DESCRIPTION: Clone the analysis framework workspace and generate fastq files.  
-##              Remove analysis framework directory at the end
-##              This pipeline should be run from within the BaseCalls directory
+##              Remove analysis framework directory at the end.
+##              This pipeline should be run from within the BaseCalls directory.
+##              If 2 parameters are provided, they will override the default parameters for
+##              fastq output directory and sample sheet file.
 ##
-## USAGE: NGS.pipeline.hiseq.fastq.lite.sh
+## USAGE: NGS.pipeline.hiseq.fastq.lite.sh [Fastq] [SampleSheet.csv]
 ##
 ## OUTPUT: Directory containing the outputs of basecalls by casava software, in fastq format
 ##
 
+# Check input parameters
+FASTQDIR=Fastq
+SAMPLESHEET=SampleSheet.csv
+if [ $# -eq 2 ]
+then
+  FASTQDIR=$1
+  SAMPLESHEET=$2
+fi
+
+# Analysis framework directory to be deleted after basecalling
 NGS_ANALYSIS_DIR=ngs-analysis
 
 # Run base caller script
-NGS.pipeline.hiseq.fastq.sh
+NGS.pipeline.hiseq.fastq.sh $FASTQDIR $SAMPLESHEET
 
 # Copy configureBclToFastq.pl script log to BaseCalls directory (cwd)
 mv $NGS_ANALYSIS_DIR/data/configureBclToFastq.log .
--- pipelines/NGS.pipeline.hiseq.fastq.sh
+++ pipelines/NGS.pipeline.hiseq.fastq.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 ##
 ## DESCRIPTION: Clone the analysis framework workspace and generate fastq files.
-##              This pipeline should be run from within the BaseCalls directory
+##              This pipeline should be run from within the BaseCalls directory.
+##              If 2 parameters are provided, they will override the default parameters for
+##              fastq output directory and sample sheet file.
 ##
-## USAGE: NGS.pipeline.hiseq.fastq.sh
+## USAGE: NGS.pipeline.hiseq.fastq.sh [Fastq] [SampleSheet.csv]
 ##
 ## OUTPUT: Directory containing the outputs of basecalls by casava software, in fastq format
 ##
 
+# Check input parameters
+FASTQDIR=Fastq
+SAMPLESHEET=SampleSheet.csv
+if [ $# -eq 2 ]
+then
+  FASTQDIR=$1
+  SAMPLESHEET=$2
+fi
 
+# Set source repository and new (cloned) repository paths
 NGS_REPOSITORY=$HOME/src/ngs-analysis
 NGS_ANALYSIS_DIR=ngs-analysis
 
@@ -18,4 +29,4 @@ cd $NGS_ANALYSIS_DIR
 
 # Run basecall
 source setup.sh
-casava.bcl2fastq.hiseq.sh ../ ../Fastq ../SampleSheet.csv
+casava.bcl2fastq.hiseq.sh ../ ../$FASTQDIR ../$SAMPLESHEET
