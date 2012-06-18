02708c4b67cb2b18d58c4b87bbc63b215ba41223
Jin <jjinking@gmail.com>
2012-06-18 10:54:00 +0900

    Modified permissions.share.sh

--- modules/util/permissions.share.sh
+++ modules/util/permissions.share.sh
@@ -3,7 +3,7 @@
 ## DESCRIPTION: Change permissions for all the files and subdirs in a directory
 ##              To enable read/write from another userid
 ##
-## USAGE: permissions.share.sh [file_permissions [dir_permissions]]
+## USAGE: permissions.share.sh file/dir [file_permissions [dir_permissions]]
 ##
 ## OUTPUT: None
 ##
@@ -11,14 +11,23 @@
 # Load analysis config
 source $NGS_ANALYSIS_CONFIG
 
+# Check usage
+usage_min 1 $# $0
+
 # Get params and set defaults
-PERM_FILE=$1
-PERM_DIR=$2
+TARGET=$1
+PERM_FILE=$2
+PERM_DIR=$3
 PERM_FILE=${PERM_FILE:=644}
 PERM_DIR=${PERM_DIR:=755}
 
 # Set permissions
-echo "Recursively setting file permissions to $PERM_FILE"
-find . -type f -exec chmod 0$PERM_FILE {} \;
-echo "Recursively setting directory permissions to $PERM_DIR"
-find . -type d -exec chmod 0$PERM_DIR {} \;
+if [ -d $TARGET ]; then
+  chmod $PERM_DIR $TARGET
+  echo "Recursively setting file permissions to $PERM_FILE"
+  find . -type f -exec chmod 0$PERM_FILE {} \;
+  echo "Recursively setting directory permissions to $PERM_DIR"
+  find . -type d -exec chmod 0$PERM_DIR {} \;
+else
+  chmod $PERM_FILE $TARGET
+fi
