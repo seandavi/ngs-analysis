d4484babf28a7499e2f3ac9738e02880b008c5f1
jin <adminrig@node01.local>
2012-07-04 13:23:43 +0900

    Fixed download_gatk_resource_bundle.sh

--- modules/util/download_gatk_resource_bundle.sh
+++ modules/util/download_gatk_resource_bundle.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ## 
 ## DESCRIPTION:   Download resource bundle from GATK ftp site
+##                Be sure to get the ftp path by right clicking on a file, not the url
 ##
 ## USAGE:         download_gatk_resource_bundle.sh path/to/ftp/directory filenamesfile [TARGET_DIR]
 ##
@@ -11,20 +12,22 @@
 source $NGS_ANALYSIS_CONFIG
 
 # Usage check:
-usage 2 $# $0
+usage_min 2 $# $0
 
 FTPDIR=$1
-FILESLIST=$2
+FILESLIST=$PWD/$2
 TARGETDIR=$3
 TARGETDIR=${TARGETDIR:='.'}
 
 # If target dir does not exist, then create it
-if [ ! -d $TARGET_DIR ]
+if [ ! -d $TARGETDIR ]
 then
-  mkdir $TARGET_DIR
+  mkdir $TARGETDIR
 fi
 
 # Download the files
+cd $TARGETDIR
 for filename in `cat $FILESLIST`; do
-  echo $filename
+  wget $FTPDIR/$filename
+  sleep 5m
 done
