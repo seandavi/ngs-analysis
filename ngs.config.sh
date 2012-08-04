9b690c6de7585b09736822e71e28c5c1528458ca
Jin <jjinking@gmail.com>
2012-08-04 20:36:36 +0900

    Modified config file to separate gatk resources b37 and hg19 versions

--- ngs.config.sh
+++ ngs.config.sh
@@ -25,22 +25,38 @@ export BEDTOOLS_PATH=path/to/bedtools_dir
 export FASTQC=path/to/fastqc
 export MUSIC=path/to/music
 
-# Resources
-export REF=path/to/human_g1k_v37.fasta
+# GATK Resource Bundle
+export B3x=path/to/gatk/resource-bundle/b37
+export B3x_REF=$B3x/human_g1k_v37.fasta
+export B3x_DBSNP_VCF=$B3x/dbsnp_135.b37.vcf
+export B3x_MILLS_INDELS_VCF=$B3x/Mills_and_1000G_gold_standard.indels.b37.vcf
+export B3x_MILLS_INDELS_SITES_VCF=$B3x/Mills_and_1000G_gold_standard.indels.b37.sites.vcf
+export B3x_1000G_INDELS_PHASE1_VCF=$B3x/1000G_phase1.indels.b37.vcf
+export B3x_HAPMAP_VCF=$B3x/hapmap_3.3.b37.vcf
+export B3x_HAPMAP_SITES_VCF=$B3x/hapmap_3.3.b37.sites.vcf
+export B3x_OMNI1000_VCF=$B3x/1000G_omni2.5.b37.vcf
+export B3x_OMNI1000_VCF=$B3x/1000G_omni2.5.b37.sites.vcf
+
+export HG=path/to/gatk/resource-bundle/hg19
+export HG_REF=$HG/ucsc.hg19.fasta
+export HG_DBSNP_VCF=$HG/dbsnp_135.hg19.vcf
+export HG_DBSNP_SITES_VCF=$HG/hapmap_3.3.hg19.sites.vcf
+export HG_MILLS_INDELS_VCF=$HG/Mills_and_1000G_gold_standard.indels.hg19.vcf
+export HG_MILLS_INDELS_SITES_VCF=$HG/Mills_and_1000G_gold_standard.indels.hg19.sites.vcf
+export HG_1000G_INDELS_PHASE1_VCF=$HG/1000G_phase1.indels.b37.vcf
+export HG_HAPMAP_VCF=$HG/hapmap_3.3.b37.vcf
+export HG_HAPMAP_SITES_VCF=$HG/hapmap_3.3.b37.sites.vcf
+export HG_OMNI1000_VCF=$HG/1000G_omni2.5.b37.vcf
+export HG_OMNI1000_VCF=$HG/1000G_omni2.5.b37.sites.vcf
+
+# Other Resources
 export UCSC_REFFLAT=path/to/refFlat.txt
 export UCSC_DBSNP=path/to/snp135.txt
-export DBSNP_VCF=path/to/dbsnp_135.b37.vcf
-export MILLS_DEVINE_INDEL_VCF=path/to/Mills_Devine_2hit.indels.b37.vcf
-export MILLS_DEVINE_INDEL_SITES_VCF=path/to/Mills_Devine_2hit.indels.b37.sites.vcf
-export INDEL_1000G_PHASE1_VCF=path/to/1000G_phase1.indels.b37.vcf
-export HAPMAP_VCF=path/to/hapmap_3.3.b37.vcf
-export HAPMAP_SITES_VCF=path/to/hapmap_3.3.b37.sites.vcf
-export OMNI1000_VCF=path/to/1000G_omni2.5.b37.sites.vcf
 export SURESELECT_BED=path/to/SureSelect_All_Exon_50mb_with_annotation_hg19_bed
 export SURESELECT_INTERVAL=path/to/SureSelect_All_Exon_50mb_with_annotation_hg19_bed.exceptChrUn.intervals
 
 #=====================================================================================
-# Developers only
+# Developers Only
 
 # NGS Analysis Pipeline Framework Tools
 source $NGS_ANALYSIS_DIR/lib/bash/bash_fnc.sh
@@ -50,6 +66,8 @@ export PATH=$PATH:$NGS_ANALYSIS_DIR/modules/seq
 export PATH=$PATH:$NGS_ANALYSIS_DIR/modules/somatic
 export PATH=$PATH:$NGS_ANALYSIS_DIR/modules/util
 export PATH=$PATH:$NGS_ANALYSIS_DIR/modules/variant
+export JAVAJAR2G='java -Xmx2g -Djava.io.tmpdir='$PWD' -jar'
+export JAVAJAR4G='java -Xmx4g -Djava.io.tmpdir='$PWD' -jar'
 export JAVAJAR8G='java -Xmx8g -Djava.io.tmpdir='$PWD' -jar'
 export JAVAJAR16G='java -Xmx16g -Djava.io.tmpdir='$PWD' -jar'
 export JAVAJAR32G='java -Xmx32g -Djava.io.tmpdir='$PWD' -jar'
