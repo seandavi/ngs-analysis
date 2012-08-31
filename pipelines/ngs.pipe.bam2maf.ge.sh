315cc4e863056bd8c285be47256df16381af8eb9
jin <adminrig@hpc01.local>
2012-08-31 11:47:47 +0900

    Modified bam2maf pipeline and deleted single node script

--- pipelines/ngs.pipe.bam2maf.ge.sh
+++ pipelines/ngs.pipe.bam2maf.ge.sh
@@ -4,7 +4,12 @@
 ##                Use grid engine using qsub
 ##                Bamlist should be given in the format specified by MuSiC (WUSTL)
 ##
-## USAGE:         ngs.pipe.bam2maf.ge.sh bamlist ref.fasta maf_out_prefix [snpeff_genome_version(default GRCh37.64)]
+## USAGE:         ngs.pipe.bam2maf.ge.sh
+##                                       bamlist
+##                                       ref.fa
+##                                       maf_out_prefix
+##                                       tumor_purity
+##                                       snpeff_genome_version(i.e. 37.64)
 ##
 ## OUTPUT:        maf_out_prefix.maf
 ##                VarScan output in varscan/ directory
@@ -14,13 +19,14 @@
 source $NGS_ANALYSIS_CONFIG
 
 # Check correct usage
-usage_min 3 $# $0
+usage 5 $# $0
 
 # Process input parameters
 BAMLIST=$1
 REFEREN=$2
 OUT_PRE=$3
-SNPEFFV=$4
+TPURITY=$4
+SNPEFFV=$5
 
 # Create temporary directory
 RNUM=$RANDOM
@@ -37,7 +43,6 @@ $NGS_ANALYSIS_DIR/pipelines/ngs.pipe.mpileup.ge.sh $TMPDIR/bamfileslist $REFEREN
 # Run varscan, annotate, and create maf files
 mkdir varscan
 SOMATIC_PVAL=0.05
-TUMOR_PURITY=1.0
 GENE2ENTREZ=$NGS_ANALYSIS_DIR/resources/gene2entrezid
 for bamfiles in `sed 's/\t/:/g' $BAMLIST`; do
   SAMPL=`echo $bamfiles | cut -f1 -d':'`
@@ -56,7 +61,7 @@ for bamfiles in `sed 's/\t/:/g' $BAMLIST`; do
           $BAM_T.mpileup                                                        \
           varscan/$SAMPL                                                        \
           $SOMATIC_PVAL                                                         \
-          $TUMOR_PURITY
+          $TPURITY
 
   # Convert varscan snp output to maf
   $QSUB varscan.vcf2maf                                                         \
