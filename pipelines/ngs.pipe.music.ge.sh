d3411f1df8f8a35080e3cb158e93db0b22ce9800
jin <adminrig@node01.local>
2012-09-12 14:23:45 +0900

    Added genome music environment variables script

--- pipelines/ngs.pipe.music.ge.sh
+++ pipelines/ngs.pipe.music.ge.sh
@@ -50,6 +50,7 @@ cut -f1 $MAFFILE | sed 1d | sort -u | sed '/^$/d' | $PYTHON $NGS_ANALYSIS_DIR/mo
 assert_normal_exit_status $? "Error generating subset of roi for genes in the maf file. Exiting"
 
 # Compute bases covered
+~/bin/genome.env.sh
 echo 'Running bmr calc-covg'
 music.bmr.calc_covg.ge.sh $BAMLIST $TMPDIR/roi.bed $OUT_DIR $REFEREN 20
 
